8967ca394d958fe804af27fd3dfd403415308320
Tim Pavlov <tema.pavlov.2000@mail.ru>
2019-07-25 20:14:15 +0300

    Front end changes
    Base Angular components were created in previous commit, common colors and fonts added, index.html modified, common styles added, Root component were modified

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AppointmentFormComponent} from './appointment/appointment-form/appointment-form.component';
+import {AppointmentComponent} from './appointment/appointment.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/appointment-form',
+    component: AppointmentFormComponent,
+  },
+  {
+    path: '/appointments',
+    component: AppointmentComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppointmentFormComponent } from './appointment/appointment-form/appointment-form.component';
+import { AppointmentComponent } from './appointment/appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppointmentFormComponent,
+    AppointmentComponent
   ],
   imports: [
     BrowserModule,
